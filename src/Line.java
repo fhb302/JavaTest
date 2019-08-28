e1dae7106a5e7f37b9e4c2e1bae361f8ed8e005e
bingoner <983093140@qq.com>
2019-08-28 17:44:11 +0800

    第一节课实验内容，实验了一个长度计算

---
+++ src/Line.java
@@ -0,0 +1,28 @@
+import mypackege.Point;
+//import javax.sound.sampled.Line;
+import java.util.*;
+
+public class Line {
+    public Point point1, point2;
+
+    public Line(Point point1, Point point2) {
+        this.point1 = point1;
+        this.point2 = point2;
+    }
+
+    public double length() {
+        int a = point1.x - point2.x, b = point1.y - point2.y;
+        return Math.sqrt(a * a + b * b);
+    }
+
+    public String toString() {
+        return "一条直线， 起点" + point1.toString() + ",终点" + point2.toString() + "，长度" + String.format("%1.2f", length());
+    }
+}
+
+class Line_ex {
+    public static void main(String[] args) {
+        Point point1 = new Point(), point2 = new Point(40, 30);
+        System.out.println(new Line(point1, point2).toString());
+    }
+}
